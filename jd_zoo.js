b48cd138861765e59232a60993a138b0e9184dd2
vemisem2gmail.com <vemisem2gmail.com>
2021-06-16 13:45:48 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -767,7 +767,7 @@ function zoo_pk_getHomeData(inviteId = "",timeout = 0) {
       $.post(url, async (err, resp, data) => {
         try {
           if (inviteId !== "") {
-            await $.getScript("https://raw.githubusercontent.com/colakele/jd/main/code/zoo.txt").then((text) => (shareCodeList = text ? text.split('\n') : []))
+            await $.getScript("https://mirror.ghproxy.com/https://raw.githubusercontent.com/colakele/jd/main/code/zoo.txt").then((text) => (shareCodeList = text ? text.split('\n') : []))
             for (let i in shareCodeList) {
               if (shareCodeList[i]) await zoo_pk_assistGroup(shareCodeList[i]);
             }
