3cc79d398bf3e7e26d74eb99462817ee2c8375fc
colakele <85920815+colakele@users.noreply.github.com>
2021-06-17 08:40:55 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -662,7 +662,7 @@ function zoo_getHomeData(inviteId= "",timeout = 0) {
                         //if (data.data.result.homeMainInfo.raiseInfo.buttonStatus === 1 )
                         if ( data.data.result.homeMainInfo.raiseInfo.buttonStatus === 1 && parseInt(data.data.result.homeMainInfo.raiseInfo.totalScore) >= parseInt(data.data.result.homeMainInfo.raiseInfo.nextLevelScore) ) await zoo_raise(1000)
                         if (date.getHours() >= 0 && date.getHours() < 1) {
-                            await zoo_getHomeData('ZXTKT0225KkcRBka_FPTJBjzkv9YfAFjRWn6-7zx55awQ');//ZXTKT0225KkcRBka_FPTJBjzkv9YfAFjRWn6-7zx55awQ
+                            await zoo_getHomeData('ZXTKT0225KkcRU0b9FDQIBKnxqRecwFjRWn6-7zx55awQ');//ZXTKT0225KkcRBka_FPTJBjzkv9YfAFjRWn6-7zx55awQ
                         }
                         await zoo_getTaskDetail()
                         await zoo_getTaskDetail("","app")
