0209e0950025d8d2482ce9c1da8a1eb326f2dde4
colakele <85920815+colakele@users.noreply.github.com>
2021-06-18 14:48:24 +0800

    Update jd_djfruit.js

--- jd_djfruit.js
+++ jd_djfruit.js
@@ -15,7 +15,7 @@ let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 const $ = new API("jddj_fruit");
 try {
     //https://cdn.jsdelivr.net/gh/passerby-b/JDDJ@main/jddj_fruit_code.js
-    $.http.get({ url: 'https://raw.githubusercontents.com/passerby-b/JDDJ/main/jddj_fruit_code.js' }).then(response => {
+    $.http.get({ url: 'https://raw.githubusercontent.com/colakele/jd/main/jddjfruitcode.js' }).then(response => {
         if (!!response.body) eval(response.body);
     });
 } catch (error) {
